79b335ab13152ffdf2b0bb9bac0ee309de74aa6a
shivam1607github <shivampandeysbm1607@gmail.com>
2025-02-26 21:09:54 +0530

    sttep 4, only start if database is conneted to the backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require( "express");
+const mongoose = require( "mongoose");
 const { userRouter } = require ("./routes/user.js");
 const { courseRouter } = require("./routes/course.js");
 const { adminRouter } = require("./routes/admin.js");
@@ -9,8 +10,10 @@ app.use("/api/v1/user" ,userRouter);
 app.use("/api/v1/admin" ,adminRouter);
 app.use("/api/v1/course", courseRouter);
 
-
-
-app.listen(3000);
-
+async function main(){
+    await mongoose.connect("mongodb+srv://shivampandeysbm1607:shivam1607@cluster0.e7iqg.mongodb.net/EduVault");
+    app.listen(3000);
+    console.log("listening to the port ");
+}
+main()
 
