d2279acdbcddb07fc48924a65c10d93875cb36c0
shivam1607github <shivampandeysbm1607@gmail.com>
2025-07-05 00:59:04 +0530

    added fronted to the eduvault and connect the frontend with backend

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-require('dotenv').config()         //dotenv code 
-console.log("MONGO_URL:", process.env.MONGO_URL);  // Debugging output         
+require('dotenv').config()         //dotenv code
+console.log("MONGO_URL:", process.env.MONGO_URL);  // Debugging output
 const express = require( "express");
+const cors = require('cors');      // <--- ADD THIS LINE
 const mongoose = require( "mongoose");
 const { userRouter } = require ("./routes/user.js");     //express - routers 
 const { courseRouter } = require("./routes/course.js");
 const { adminRouter } = require("./routes/admin.js");
 const app = express();
+app.use(cors());                   // <--- AND THIS LINE
 app.use(express.json());
 
 app.use("/api/v1/user" ,userRouter);
